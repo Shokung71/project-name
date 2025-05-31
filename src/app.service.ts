e7b1064e9d0b834f05125c6e5d3128167cd52f3e
Bancha Wasanasitthi <bancha4371@gmail.com>
2025-05-31 14:18:50 +0700

    Remove commented explanations from getHello() method and clean up ESLint rules

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,9 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): any {
-  // any คือการใช้ type ที่ไม่ระบุประเภทที่แน่นอน
-  // // ใช้เมื่อไม่ต้องการระบุประเภทที่แน่นอนของข้อมูลที่ส่งกลับ
-    // คืนค่าเป็นอ็อบเจ็กต์ที่มีชื่อและรหัสประจำตัว
     return {
       name: "Bancha Wasanasitthi",
       id: 66106030,
